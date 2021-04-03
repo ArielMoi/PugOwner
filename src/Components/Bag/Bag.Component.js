a000e19d9816afc0a72d1748a2fa29593b9bcb75
Ariel Moise <ariel.m23@gmail.com>
2021-04-03 12:08:23 +0300

    home page

--- src/Components/Bag/Bag.Component.js
+++ src/Components/Bag/Bag.Component.js
@@ -1,29 +1,43 @@
-import Item from '../Item/Item.Component'
+import "./Bag.css";
+import Item from "../Item/Item.Component";
 
 const Bag = (props) => {
-    return (
+  const toys = [];
+  const foods = [];
+
+  Object.entries(props.bagObj.food).forEach(([amount, item]) => {
+    Array(Number(amount))
+      .fill()
+      .forEach(() => {
+        // creates array to iterate over with map
+        foods.push(<Item imgUrl={item[0]} product={item[1]} />); // doesn't return for some reason
+      });
+  });
+
+  Object.entries(props.bagObj.toys).forEach(([amount, item]) => {
+    Array(Number(amount))
+      .fill()
+      .forEach(() => {
+        // creates array to iterate over with map
+        toys.push(<Item imgUrl={item[0]} product={item[1]} />); // doesn't return for some reason
+      });
+  });
+
+  return (
+    <div className='bag' style={{visibility:props.visibility}}>
+      <h1>YOUR BAG</h1>
+
       <div>
-        <h1>YOUR BAG</h1>
-
-        <div>
-          <h3>Food</h3>
-          <div className="bag-foods">
-            {props.bagObj.food.map((item) => {
-              <Item imgUrl={item.imgUrl} product={item.product} />;
-            })}
-          </div>
-        </div>
-
-        <div>
-          <h3>TOYS</h3>
-          <div className="bag-toys">
-            {props.bagObj.toys.map((item) => {
-              <Item imgUrl={item.imgUrl} product={item.product} />;
-            })}
-          </div>
-        </div>
+        <h3 className="bag-type-header">Food</h3>
+        <div className="bag-type">{foods}</div>
       </div>
-    );
-}
+
+      <div>
+        <h3 className="bag-type-header">Toys</h3>
+        <div className="bag-type">{toys}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Bag;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,14 +4,23 @@ import Bag from '../../Components/Bag/Bag.Component'
 import Button from '../../Components/Button/Button.Component'
 import bagImg from "../../img/bag.png";
 import { useState, useEffect } from "react";
+import appleImg from '../../img/apple.jpg'
+import bananaImg from '../../img/banana.jpg'
+import bawlImg from '../../img/bawl-food.jpg'
+import chickenImg from '../../img/chicken.jpg'
+import ballToy from '../../img/ball.jpg'
 
+const tempBag = {
+  food: {1: [appleImg, 'apple'], 5: [bananaImg, 'banana'], 2: [bawlImg, 'bawl'],},
+  toys: {2: [ballToy, 'ball']}
+}
 
 const Home = () => {
 
   const [bagVisibility, setBagVisibility] = useState('hidden');
 
   const clickOnBag = () => {
-    bagVisibility == "hidden"
+    bagVisibility === "hidden"
       ? setBagVisibility("visible")
       : setBagVisibility("hidden");
   }
@@ -26,6 +35,7 @@ const Home = () => {
           happinessAmount="70"
         />
         <Button img={bagImg} onClickFunc={clickOnBag}/>
+        <Bag bagObj={tempBag} visibility={bagVisibility}/>
       </div>
     );
 }
